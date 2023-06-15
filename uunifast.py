c9fd7742daeaa16a720692278be7fb3fba56f157
Rodrigo <rodrigo.ornellas@yahoo.com.br>
2023-06-15 11:44:32 -0300

    algoritmo uunifasts

---
+++ uunifast.py
@@ -0,0 +1,45 @@
+import random
+from funcoes import generateTaskFromUtilization
+
+
+def uunifast(n, U):
+    vectU = []
+    sumU = U
+    for i in range(1, n):
+        nextSumU = sumU * (random.uniform(0, 1)**(1/(n-i)))
+        vectU.append(sumU-nextSumU)
+        sumU = nextSumU
+    vectU.append(sumU)
+    AllSum = 0
+
+    #summation over all utilization for finding global utilization
+    for i in range(len(vectU)):
+        AllSum += vectU[i]
+
+    return AllSum, vectU
+
+
+def uunifasts(N, n, U):
+    # if there is a file at the moment, overwrite on it 
+    setList = []
+    # will be used for configuration propose
+    counter = 1
+    while(counter <= N):
+        Sum, Vect = uunifast(n, U)
+        # Because of rounding errors, utilization may not be same as given U
+        # for example, 0.9999 is unacceptable when utilization(U) is equal to 1
+        if(Sum == U):
+            # call function that makes the
+            taskSet = generateTaskFromUtilization(Vect)
+            setList.append(taskSet)
+            #counters seperates outputs of each runs
+
+            counter += 1
+    return setList
+
+# run 1000 time , make 100 job in each list and with 1 utilization
+# set proper values here 
+'''RUN = 10   # how many times we run unifast algorithm
+JOB_NUMBERS = 6 #task number in each list 
+UTILIZATION = 1 #Utilization
+uunifasts(RUN, JOB_NUMBERS, UTILIZATION)'''
