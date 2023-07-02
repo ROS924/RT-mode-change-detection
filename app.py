32b66480daca9098db05e105b54850fdc7081188
Rodrigo <rodrigo.ornellas@yahoo.com.br>
2023-07-02 02:15:17 -0300

    chamada para gerar tarefas: app -> gerar_tarefas

--- app.py
+++ app.py
@@ -1,43 +1,12 @@
-from uunifast import uunifasts, generateTaskSet
-from classes.task import Task
+from classes import opmode,server
+from series_temporais_base.tarefas import t1, t2, t3
 
-tarefa1 = generateTaskSet(5,1)
-tarefa1_1 = tarefa1[0]
+server1ModeList = [opmode.OPMODE(10,100),opmode.OPMODE(30,20),opmode.OPMODE(10,200),opmode.OPMODE(7,100)]
+server2ModeList = [opmode.OPMODE(15,50),opmode.OPMODE(25,100),opmode.OPMODE(8,40),opmode.OPMODE(9,60)]
+server3ModeList = [opmode.OPMODE(50,100),opmode.OPMODE(16,40),opmode.OPMODE(21,60),opmode.OPMODE(10,50)]
+server4ModeList = [opmode.OPMODE(35,50),opmode.OPMODE(110,200),opmode.OPMODE(30,50),opmode.OPMODE(15,50)]
+server5ModeList = [opmode.OPMODE(56,70),opmode.OPMODE(30,40),opmode.OPMODE(65,100),opmode.OPMODE(40,100)]
 
-tarefa2 = generateTaskSet(5,1)
-tarefa2_1 = tarefa2[0]
-
-tarefa3 = generateTaskSet(5,1)
-tarefa3_1 = tarefa3[0]
-
-print("TAREFA 1")
-for i in tarefa1:
-    task = Task(i.executionTime,i.period,i.deadline)
-    executionTime = task.getExecutionTime()
-    deadline = task.getDeadline()
-
-    par = [executionTime, deadline]
-
-    print(f"{par}\n")
-
-
-print("TAREFA 2")
-for i in tarefa2:
-    task = Task(i.executionTime,i.period,i.deadline)
-    executionTime = task.getExecutionTime()
-    deadline = task.getDeadline()
-
-    par = [executionTime, deadline]
-
-    print(f"{par}\n")
-
-
-print("TAREFA 3")
-for i in tarefa3:
-    task = Task(i.executionTime,i.period,i.deadline)
-    executionTime = task.getExecutionTime()
-    deadline = task.getDeadline()
-
-    par = [executionTime, deadline]
-
-    print(f"{par}\n")
+server1 = server.SERVER(1,t1,server1ModeList)
+server2 = server.SERVER(2,t2,server2ModeList)
+server3 = server.SERVER(3,t3,server3ModeList)
