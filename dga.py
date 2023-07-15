e8c32de3c57e876c4e8c19177c3748b990bcdeea
Rodrigo <rodrigo.ornellas@yahoo.com.br>
2023-07-14 23:21:17 -0300

    implemenmtação do dga

--- dga.py
+++ dga.py
@@ -1,4 +1,4 @@
-from classes import par, server, opmode
+from classes import par
 from operator import attrgetter
 
 def createParSet(serverList):
@@ -16,6 +16,66 @@ def createParSet(serverList):
 def DGA(serverList):
 
     parSet = createParSet(serverList)
-    ordenedParSet = sorted(parSet, key = attrgetter('mode.utilizacao'), reverse = True)
+    usable = []
+    limite = len(parSet) - len(serverList)
+
+    for i in range(limite):
+        usable.append(parSet[i])
+
+    ordenedParSet = sorted(usable, key = attrgetter('mode.utilizacao'), reverse = True)
     finalOrdParSet = sorted(ordenedParSet, key=attrgetter('mode.beneficio'),reverse = True)
 
+    w = []
+
+    somaU = 0
+
+    for serv in serverList:
+        U = serv.currentMode.utilizacao
+        somaU += U
+
+    b = 1 - somaU
+    t = 1
+
+    while (t <= limite) and (len(w) < len(serverList)):
+        parId = 1
+        Sw = []
+        for pair in w:
+            Sw.append(pair.server)
+
+        for serv in serverList:
+            if serv.id == t:
+                if ((serv in Sw) == False):
+                    modo = None
+                    modoMark = serv.modeList[0]
+
+                    for mdo in serv.modeList:                   
+                        if mdo.id == t:
+                            modo = mdo
+                            break
+                    
+                    deltaU = modo.utilizacao - modoMark.utilizacao
+                    if deltaU <= b:
+                        w.append(par.PAR(parId,serv,modo))
+                        b = b - deltaU
+                
+                break
+                
+        t += 1
+    
+
+    sW = []
+    for pair in w:
+        sW.append(pair.server)
+
+    S_Sw = []
+    
+    for serv in serverList:
+        if (serv in sW) == False:
+            S_Sw.append(serv)
+
+    nParId = w[len(w)-1].id + 1
+
+    for servidor in S_Sw:
+        w.append(par.PAR(nParId,servidor, servidor.modeList[0]))
+
+    return w
